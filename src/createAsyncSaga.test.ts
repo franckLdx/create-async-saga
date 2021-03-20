aced9f8245f0715c460f2a51685b9a3f579f0a19
Utilisateur <franck.ledoux.pro@gmail.com>
2021-03-20 21:35:51 +0100

    add tests

--- src/createAsyncSaga.test.ts
+++ src/createAsyncSaga.test.ts
@@ -2,22 +2,79 @@ import { createAsyncSaga } from './createAsyncSaga';
 import { testSaga } from 'redux-saga-test-plan';
 
 describe('createAsyncSaga', () => {
-  it('Should execute sucessful liefecycle  with no arg', () => {
+  describe('lifecycle', () => {
+    it('Should execute sucessful liefecycle', () => {
+      const typePrefix = "SagaAfrica";
+      const message = 'hello world';
+      const asyncSaga = createAsyncSaga(typePrefix, function* () { return message });
+      testSaga(asyncSaga.asyncSaga, { type: typePrefix, payload: undefined })
+        .next()
+        .put({
+          type: `${typePrefix}/pending`,
+          payload: undefined,
+          meta: { arg: undefined }
+        })
+        .next()
+        .put({
+          type: `${typePrefix}/fulfilled`,
+          payload: message,
+          meta: { arg: undefined }
+        });
+    });
+
+    it('Should execute failed liefecycle', () => {
+      const typePrefix = "SagaAfrica";
+      const error = new Error("BOOM");
+      const asyncSaga = createAsyncSaga(typePrefix, function* () { throw error; });
+      testSaga(asyncSaga.asyncSaga, { type: typePrefix, payload: undefined })
+        .next()
+        .put({
+          type: `${typePrefix}/pending`,
+          payload: undefined,
+          meta: { arg: undefined }
+        })
+        .next()
+        .put({
+          type: `${typePrefix}/rejected`,
+          payload: error,
+          meta: { arg: undefined }
+        });
+    });
+  });
+  describe('condition', () => {
     const typePrefix = "SagaAfrica";
     const message = 'hello world';
-    const asyncSaga = createAsyncSaga(typePrefix, function* () { return message });
-    testSaga(asyncSaga.asyncSaga, { type: typePrefix, payload: undefined })
-      .next()
-      .put({
-        type: `${typePrefix}/pending`,
-        payload: undefined,
-        meta: { arg: undefined }
-      })
-      .next()
-      .put({
-        type: `${typePrefix}/fulfilled`,
-        payload: message,
-        meta: { arg: undefined }
-      });
+    function* doMessage() { return message };
+
+    it('should execute saga', () => {
+      const asyncSaga = createAsyncSaga(
+        typePrefix,
+        doMessage,
+        { condition: function* () { return true } }
+      );
+      testSaga(asyncSaga.asyncSaga, { type: typePrefix, payload: undefined })
+        .next()
+        .put({
+          type: `${typePrefix}/pending`,
+          payload: undefined,
+          meta: { arg: undefined }
+        })
+        .next()
+        .put({
+          type: `${typePrefix}/fulfilled`,
+          payload: message,
+          meta: { arg: undefined }
+        });
+    });
+    it('should not execute saga', () => {
+      const asyncSaga = createAsyncSaga(
+        typePrefix,
+        doMessage,
+        { condition: function* () { return false } }
+      );
+      testSaga(asyncSaga.asyncSaga, { type: typePrefix, payload: undefined })
+        .next()
+        .isDone();
+    });
   });
 });
